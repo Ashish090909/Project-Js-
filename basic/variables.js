74ccfd55ee8e193b519b596b9117e9d66c69501a
Ashish090909 <126828005+Ashish090909@users.noreply.github.com>
2025-01-11 14:28:53 +0000

    variables basic

---
+++ basic/variables.js
@@ -0,0 +1,15 @@
+const abcd = 1332
+let bcaa = "fcuk"
+var acaa ="1234"
+acci="jai ho"
+let acuy;
+
+bcaa="fghu"
+acaa="345"
+acci="jai"
+
+console.table([abcd,bcaa,acaa,acci])
+
+/*
+var should not be used due to issue in block and functional scope and thus we use let in javascript
+*/
