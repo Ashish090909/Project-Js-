be9052911dce2e21e3ac5151ca2b5bc70710a132
Ashish090909 <126828005+Ashish090909@users.noreply.github.com>
2025-01-12 13:36:36 +0000

    data-types

---
+++ basic/datatypes.js
@@ -0,0 +1,16 @@
+"Use Stricts" // Use New version of java scripts
+
+//alert (3+3) we are using node js not browser and thus it will throw error
+
+let name = "fcuk"
+let age = 3
+let istrue = false
+
+//number -> ranges from 2 to 2^53 or similar to that 52 i dont remmeber
+//bigint -> big range of integer
+//string -> ""
+//boolean -> true/false
+//null -> standalone value will return data type object when printed through console.log(typeof")
+//undefined->undefined
+//symbol -> unique
+console.log(typeof""); //-> prints type of data used
